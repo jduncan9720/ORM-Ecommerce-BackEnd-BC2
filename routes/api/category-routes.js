f820113936823d5c375b026d3c3fdc1a015f05a3
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-12-12 16:50:31 -0700

    working on tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,28 +20,27 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-  const categoryData = await Category.findByPk(req.params.id, {
-    include: [{ model: Product }],
-  });
-  if (!categoryData){
-    res.status(404).json({ message: "No category found with that id!"});
-    return;
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
   }
-  res.status(200).json(categoryData);
-} catch (err) {
-  res.status(500).json(err)
-}
 });
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create(req.body)
-  .then((newCategory) => {
-   res.json(newCategory)
-  })
-  .catch((err) => {
-    res.json(err)
-  })
+  try {
+    const newCategory = Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
@@ -54,13 +53,13 @@ router.put('/:id', async (req, res) => {
     });
     console.log(categoryData)
     if (!categoryData[0]) {
-      res.status(404).json({ message: "No category with that id!"})
+      res.status(404).json({ message: "No category with that id!" })
       return;
     }
     res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -73,13 +72,13 @@ router.delete('/:id', async (req, res) => {
     });
     console.log(categoryData)
     if (!categoryData) {
-      res.status(404).json({ message: "No category with that id!"})
+      res.status(404).json({ message: "No category with that id!" })
       return;
     }
     res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 });
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,6 +35,15 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  // {
+  //   "tag_name": "Green"
+  // }
+  try{
+    const newTag = Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
